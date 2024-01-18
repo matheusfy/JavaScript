3fc8043705f52848ed4a97333e21eeb40f8409c2
matheusfy <matheusy@alunos.utfpr.edu.br>
2024-01-18 15:54:59 -0300

    adicionando exercicios JS [new]

---
+++ logica-js-projeto_inicial/app.js
@@ -0,0 +1,21 @@
+alert("Boas vindas ao nosso site!");
+
+let nome = "Lua";
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivel = 1000;
+
+alert("Erro! Preencha todos os campos");
+
+let mensagemDeErro = "Erro! Preencha todos os campos"
+
+alert(mensagemDeErro);
+
+nome = prompt("Insira seu nome de usuario");
+
+idade = prompt("Insira sua idade");
+
+if(idade > 18){
+    alert("Pode tirar a habilitação")
+}
+
