648c60d5b03a527517c5205734aa3fa3af29716e
matheusfy <matheusy@alunos.utfpr.edu.br>
2024-01-19 20:04:55 -0300

    Commit Exercicio2 [Alura]

---
+++ logica-js-aula_2/app.js
@@ -0,0 +1,79 @@
+// alert('Boas vindas ao jogo do número secreto');
+// let numeroSecreto = 9;
+// console.log(numeroSecreto)
+
+// // se chute for igual ao número secreto
+// let acerto = false;
+// let tentativas = 0;
+
+// while(!acerto){
+//     let chute = prompt('Escolha um número entre 1 e 10');
+
+//     if (chute == numeroSecreto) {
+//         alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} apos ${tentativas}`);
+//         acerto = true;
+//     } else {
+//         alert('Você errou :(. Tente novamente')
+//         tentativas++;
+//     }
+
+//     if(tentativas == 5){
+//         alert("voce extrapolou o limite de tentativas! Saindo do programa");
+//         acerto = true;
+//     }
+// }
+
+
+/////////////////////// Desafio 1
+
+// console.log("Desafio 1");
+// let contador = 1;
+
+// while(contador < 10){
+//     console.log("contador: " + contador);
+//     contador++;
+// }
+
+
+// //////////////////////// Desafio 2
+// console.log("Desafio 2");
+
+// let contador2 = 10;
+
+// while(contador2 > 0){
+//     console.log("contador: " + contador2);
+//     contador2--;
+// }
+
+// //////////////////////// Desafio 3
+
+// console.log("Desafio 3");
+
+// let regressiva = prompt("Insira um numero para contagem regressiva");
+
+// if(regressiva > 0){
+//     while(regressiva > 0){
+//         console.log("Regressiva em " + regressiva);
+//         regressiva--;
+//     }
+// } else {
+//     alert("não é possivel fazer uma regressiva com o valor indicado. Valor = " + regressiva);
+// }
+
+
+////////////////////////// Desafio 4
+
+// console.log("Desafio 4");
+
+// let contagem4 = prompt("Insira um valor para a contagem");
+// let contador4 = 0;
+
+// if (contagem4 > 0){
+//     while(contagem4 >= contador4){
+//         console.log("contando: "+ contador4);
+//         contador4++;
+//     }
+
+// } else {
+//     alert("não é possivel fazer uma regressiva com o valor indicado. Valor = " + contagem4);
+// }
